e0e4d36f04f527fce510e73026272d0ce599058a
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-10-08 20:55:56 -0700

    create something

--- exercises/first-website-react/src/components/Body.js
+++ exercises/first-website-react/src/components/Body.js
@@ -1,9 +1,15 @@
 import React from "react";
+import data from "../json/body.json";
+import Data from "./Data"
 
 function Body() {
   return (
     <div>
-      
+      {data.map(function(spot) {
+        return <Data
+              spot={spot}
+                />
+      })}
     </div>
   )
 }
--- exercises/first-website-react/src/components/Header.js
+++ exercises/first-website-react/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Header() {
+  const styles = {
+      background: `url("https://blackrockdigital.github.io/startbootstrap-clean-blog/img/home-bg.jpg") no-repeat center`,
+      backgroundSize: "cover"
+  }
+
   return (
-    <div>
-    
-    </div>
+      <div className="header-container" style={styles}>
+          <div className="header-text-container">
+              <h1>Clean Blog</h1>
+              <p>A Blog Theme by Start Bootstrap</p>
+          </div>
+      </div>
   )
 }
-
 export default Header;
