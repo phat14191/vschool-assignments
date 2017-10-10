8ae343d64be19f74e73e614047f97ef29250b52b
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-10-09 23:49:12 -0700

    review supperHero ecercise

--- exercises/super-heroes-react/src/components/App.js
+++ exercises/super-heroes-react/src/components/App.js
@@ -2,25 +2,41 @@ import React from "react";
 import SuperHero from "./SuperHero"
 import data from "./data.json"
 
-function genHeroes() {
-  return data.map((spot) => {
-    return <SuperHero
-            spot = {spot}
-            handleClick = {handleClick}
-           />
-  })
-}
-
-function handleClick(str) {
-  alert(str);
-}
-
 function App() {
   return (
     <div>
-      {genHeroes(data)}
+      {data.map(function(spot) {
+        return <SuperHero
+              spot = {spot}
+               />
+      })
+    }
     </div>
   )
-}
+};
+
+
+
 
 export default App;
+
+// function genHeroes() {
+//   return data.map((spot) => {
+//     return <SuperHero
+//             spot = {spot}
+//             handleClick = {handleClick}
+//            />
+//   })
+// }
+//
+// function handleClick(str) {
+//   alert(str);
+// }
+//
+// function App() {
+//   return (
+//     <div>
+//       {genHeroes(data)}
+//     </div>
+//   )
+// }
--- exercises/super-heroes-react/src/components/SuperHero.js
+++ exercises/super-heroes-react/src/components/SuperHero.js
@@ -2,15 +2,26 @@ import React from "react";
 
 function SuperHero(props) {
   function handleClick() {
-    alert("Name: " +  props.spot.title +
-          "!!!  Detailed: " + props.spot.details2)
+    alert("Name: " + props.spot.title + " Detailed: " + props.spot.details2)
   }
   return (
-    <div onClick = { () => {props.handleClick(props.spot.details2)}}>
-      <h1>Characters: {props.spot.title}</h1>
-      <h3>Description: {props.spot.details}</h3>
-      <img src={props.spot.photo}/>
+    <div onClick={handleClick}>
+      <h1>{props.spot.title}</h1>
+      <p>{props.spot.details}</p>
+      <img src={props.spot.photo}></img>
     </div>
   )
 }
 export default SuperHero;
+
+//   function handleClick() {
+//     alert("Name: " +  props.spot.title +
+//           "!!!  Detailed: " + props.spot.details2)
+//   }
+//   return (
+//     <div onClick = { () => {props.handleClick(props.spot.details2)}}>
+//       <h1>Characters: {props.spot.title}</h1>
+//       <h3>Description: {props.spot.details}</h3>
+//       <img src={props.spot.photo}/>
+//     </div>
+//   )
