3d9f142c65f065b9c0d845887d05da5da7360c6c
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-10-12 15:19:29 -0700

    make delete button, so when you click on it, the id from server will gone on your website and your server

--- exercises/todos-react/src/components/TodoItem.js
+++ exercises/todos-react/src/components/TodoItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function TodoItem(props) {
-  return (
-    <div>
+  return(
+    <div >
       <h1>{props.todo.title}</h1>
       <h2>{props.todo.description}</h2>
+      <button onClick={() => props.deleteTodo(props.todo._id)}>Delete</button>
+      <hr/>
     </div>
   )
 }
--- exercises/todos-react/src/components/TodoList.js
+++ exercises/todos-react/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoItem from "./TodoItem"
+import axios from "axios";
 
 class TodoList extends React.Component {
   constructor() {
@@ -7,21 +8,40 @@ class TodoList extends React.Component {
     this.state = {
       todos: []
     }
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
 
   componentDidMount() {
-    
+    axios.get("https://api.vschool.io/phat/todo").then((response) => {
+      console.log(response)
+      this.setState({
+        todos: response.data
+      });
+    });
+  }
+
+  deleteTodo(id) {
+    axios.delete(`https://api.vschool.io/phat/todo/${id}`).then(response => {
+      console.log(response.data);
+      this.setState(prevState => {
+        const newArr = prevState.todos.filter(item => item._id !== id)
+        return {
+          todos: newArr
+        }
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        {this.state.todos.map(function(item, i) {
+        {this.state.todos.map((item, i) => {
           return (
-            <TodoItem
-              todo = {item}
-              key = {item.title + i}
-            />
+              <TodoItem
+                todo = {item}
+                key = {item.title + i}
+                deleteTodo = {this.deleteTodo}
+              />
           )
         })}
       </div>
