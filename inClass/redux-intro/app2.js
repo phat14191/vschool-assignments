be32dc4e8464ffab35a2f3e37634b8a76c7bab5b
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-10-18 13:32:54 -0700

    add function removeName to take first element in state and store it in display

--- inClass/redux-intro/app2.js
+++ inClass/redux-intro/app2.js
@@ -1,11 +1,22 @@
 const redux = require("redux");
-
 // console.log(redux);
-
 const state = {
-  counter: 0
+  counter: 0,
+  contactList: [],
 }
-
+function addName(name) {
+  return {
+    type: "ADD_NAME",
+    value: name,
+  }
+}
+//remove first name
+function removeName(name) {
+  return {
+    type: "REMOVE_NAME",
+    value: name,
+  }
+}
 //tell this patcher add one
 function custom(num) {
   return {
@@ -13,13 +24,28 @@ function custom(num) {
     value: num,
   };
 }
-
 //take action take prevState also take action
 function reducer(prevState = state, action) { //prevState = state get it know what is the initial state is
   switch(action.type) {
     case "CUSTOM":
       return {
+        contactList: prevState.contactList,
         counter: prevState.counter + action.value
+      }
+    case "ADD_NAME":
+      return {             //array                  string
+        contactList: [...prevState.contactList, action.value],
+        counter: prevState.counter
+      }
+    case "REMOVE_NAME":
+        //make a copy of prevState.contactList
+        let newList = [...prevState.contactList];
+        //remove the first item from the copy
+        newList.shift();
+        //set the new copy into state
+        return {
+          counter: prevState.count,
+          contactList: newList
       }
     default:
       return prevState;
@@ -33,8 +59,11 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 //action ==> dispatch ==> reducer ==> state ==> react view change
-
-
 // thispatch is method on store
-
 store.dispatch(custom(20));
+store.dispatch(addName("hello"));
+store.dispatch(addName("Hi"));
+store.dispatch(addName("Bob"));
+store.dispatch(addName("Eric"));
+store.dispatch(removeName());
+store.dispatch(removeName());
