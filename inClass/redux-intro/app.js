d7e0e542317768d6ed36a76ff2749ec9b89b4dc0
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-10-18 10:20:00 -0700

    make redux -1

--- inClass/redux-intro/app.js
+++ inClass/redux-intro/app.js
@@ -13,6 +13,14 @@ function addOne() {
   };
 }
 
+//function subtract - 1
+function subOne() {
+  return {
+    type: "SUB_ONE",
+  };
+}
+
+
 //take action take prevState also take action
 function reducer(prevState = state, action) { //prevState = state get it know what is the initial state is
   switch(action.type) {
@@ -25,15 +33,32 @@ function reducer(prevState = state, action) { //prevState = state get it know wh
   }
 }
 
+function reducer(prevState = state, action) {
+  switch (action.type) {
+    case "SUB_ONE":
+      return {
+        counter: prevState.counter -1
+      }
+    default:
+      return prevState;
+  }
+}
+
 
 // store take couple argument
 const store = redux.createStore(reducer);
-
+const store1 = redux.createStore(reducer);
+// subtract one
 // console.log(store.getState());
 store.subscribe(() => {
   console.log(store.getState());
 });
+//action ==> dispatch ==> reducer ==> state ==> react view change
+
 
 // thispatch is method on store
 store.dispatch(addOne());
 store.dispatch(addOne());
+
+store.dispatch(subOne());
+store.dispatch(subOne());
